2949e891ae00fbb888475de7788be5b4bdd9e4d4
Ziemniakoss <rawa.przemek@gmail.com>
2019-12-11 18:27:24 +0100

    koniec wykładu

---
+++ src/main/java/testprojcztery/controllers/CManageFlashcards.java
@@ -0,0 +1,22 @@
+package testprojcztery.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CManageFlashcards {
+	@FXML
+	private Button saveChangesButton;
+	@FXML
+	private Button discardChangesButton;
+
+	@FXML
+	private void discardChangesButtonOnAction(ActionEvent actionEvent) {
+
+	}
+
+	@FXML
+	private void saveCHangesButtonOnAction(ActionEvent actionEvent) {
+
+	}
+}
